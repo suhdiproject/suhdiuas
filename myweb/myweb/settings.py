76eacf52ce0c1c64b7ed0602290cc88e0f968ad2
fatimfatim <syfatimah07@gmail.com>
2022-12-28 23:40:31 +0700

    menambah models di contaks

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'myweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['tempaltes'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
